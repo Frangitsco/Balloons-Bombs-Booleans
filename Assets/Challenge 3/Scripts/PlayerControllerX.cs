e9e21481c8fa0482db6d1f3a89b32833c9c826eb
Francesco Ponzo <mr.franceso.ponzo@gmail.com>
2023-12-20 11:45:58 +0000

    Initial commit

---
+++ Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public bool gameOver;
+
+    public float floatForce;
+    private float maxHeight = 14f; 
+    private float gravityModifier = 1.5f;
+    private Rigidbody playerRb;
+
+    public ParticleSystem explosionParticle;
+    public ParticleSystem fireworksParticle;
+
+    private AudioSource playerAudio;
+    public AudioClip moneySound;
+    public AudioClip explodeSound;
+    public AudioClip bounceSound;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+        playerAudio = GetComponent<AudioSource>();
+
+        // Apply a small upward force at the start of the game
+        playerRb.AddForce(Vector3.up * 5, ForceMode.Impulse);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // While space is pressed and player is low enough, float up
+        if (Input.GetKey(KeyCode.Space) && transform.position.y < maxHeight && !gameOver)
+        {
+            playerRb.AddForce(Vector3.up * floatForce);
+        }
+        // Check if player has reached or exceeded the maximum height
+        if (transform.position.y >= maxHeight && playerRb.velocity.y > 0)
+        {
+            // Stop upward movement by setting vertical velocity to zero
+            playerRb.velocity = new Vector3(playerRb.velocity.x, 0, playerRb.velocity.z);
+        }
+       
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // if player collides with bomb, explode and set gameOver to true
+        if (other.gameObject.CompareTag("Bomb"))
+        {
+            explosionParticle.Play();
+            playerAudio.PlayOneShot(explodeSound, 1.0f);
+            gameOver = true;
+            Debug.Log("Game Over!");
+            Destroy(other.gameObject);
+        } 
+
+        // if player collides with money, fireworks
+        else if (other.gameObject.CompareTag("Money"))
+        {
+            fireworksParticle.transform.position = transform.position;
+            fireworksParticle.Play();
+            playerAudio.PlayOneShot(moneySound, 1.0f);
+            Destroy(other.gameObject);
+
+        }
+
+            // Check if collided with the ground
+        if (other.gameObject.CompareTag("Ground") && !gameOver)
+        {
+            // Apply an upward impulse force
+            playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+            //Play sound effect
+            playerAudio.PlayOneShot(bounceSound, 1.0f);
+
+        }
+
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+I've applied my knowledge of physics, scrolling backgrounds, and special 
+effects to a balloon floating through town, picking up tokens while 
+avoiding explosives.
+Press the Space Bar to float into the air, enjoy the ride!
+ 
